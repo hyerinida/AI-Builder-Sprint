1820cb0ae006515e71912b8b15afa38ef44a8c7d
whdwon <whdwon09@gmail.com>
2026-07-29 17:29:14 +0900

    refactor: 엔티티에 Lombok 어노테이션 추가

--- backend/prism/src/main/java/com/dontgiveup/prism/config/UpstageProperties.java
+++ backend/prism/src/main/java/com/dontgiveup/prism/config/UpstageProperties.java
@@ -1,22 +1,16 @@
 package com.dontgiveup.prism.config;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "upstage")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UpstageProperties {
     private String apiKey;
     private String baseUrl;
-
-    public String getApiKey() {
-        return apiKey;
-    }
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
-    }
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-    public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl;
-    }
 }
